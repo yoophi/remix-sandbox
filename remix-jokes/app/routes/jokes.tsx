80bc62eb0b1f64b15e5448bd277da85c56addcc1
Pyunghyuk Yoo <yoophi@gmail.com>
2021-12-21 00:43:30 +0900

    ✨ NEW: jokes routes

---
+++ remix-jokes/app/routes/jokes.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "remix";
+
+export default function JokesRoute() {
+  return (
+    <div>
+      <h1>J🤪KES</h1>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
