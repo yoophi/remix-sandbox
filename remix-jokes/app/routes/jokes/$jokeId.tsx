e66d79583ebc62d328b9e12a687b861c7b610f87
Pyunghyuk Yoo <yoophi@gmail.com>
2021-12-26 00:10:15 +0900

    ✨ NEW: optimistic UI

--- remix-jokes/app/routes/jokes/$jokeId.tsx
+++ remix-jokes/app/routes/jokes/$jokeId.tsx
@@ -2,7 +2,8 @@ import type { LoaderFunction, ActionFunction, MetaFunction } from "remix";
 import { Link, useLoaderData, useCatch, redirect, useParams } from "remix";
 import type { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
-import { requireUserId } from "~/utils/session.server";
+import { getUserId, requireUserId } from "~/utils/session.server";
+import { JokeDisplay } from "~/components/joke";
 
 export const meta: MetaFunction = ({
   data,
@@ -21,9 +22,11 @@ export const meta: MetaFunction = ({
   };
 };
 
-type LoaderData = { joke: Joke };
+type LoaderData = { joke: Joke; isOwner: boolean };
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await getUserId(request);
+
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
@@ -32,7 +35,10 @@ export const loader: LoaderFunction = async ({ params }) => {
       status: 404,
     });
   }
-  const data: LoaderData = { joke };
+  const data: LoaderData = {
+    joke,
+    isOwner: userId === joke.jokesterId,
+  };
   return data;
 };
 
@@ -59,19 +65,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
 
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      <form method="post">
-        <input type="hidden" name="_method" value="delete" />
-        <button type="submit" className="button">
-          Delete
-        </button>
-      </form>
-    </div>
-  );
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
 }
 
 export function CatchBoundary() {
@@ -98,7 +92,9 @@ export function CatchBoundary() {
   }
 }
 
-export function ErrorBoundary() {
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
   const { jokeId } = useParams();
   return (
     <div className="error-container">{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
