60eef658ad2b115637560c223af9944b6edcef68
Pyunghyuk Yoo <yoophi@gmail.com>
2021-12-25 23:24:23 +0900

    ✨ NEW: SEO

--- remix-jokes/app/routes/index.tsx
+++ remix-jokes/app/routes/index.tsx
@@ -1,4 +1,5 @@
-import type { LinksFunction } from "remix";
+import type { LinksFunction, MetaFunction } from "remix";
+import { Link } from "remix";
 import stylesUrl from "../styles/index.css";
 
 export const links: LinksFunction = () => {
@@ -10,6 +11,28 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
+export const meta: MetaFunction = () => {
+  return {
+    title: "Remix: So great, it's funny!",
+    description: "Remix jokes app. Learn Remix and laugh at the same time!",
+  };
+};
+
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
--- remix-jokes/app/routes/login.tsx
+++ remix-jokes/app/routes/login.tsx
@@ -1,4 +1,4 @@
-import type { ActionFunction, LinksFunction } from "remix";
+import type { ActionFunction, LinksFunction, MetaFunction } from "remix";
 import { useActionData, json, Link, useSearchParams } from "remix";
 import { db } from "~/utils/db.server";
 import { createUserSession, login, register } from "~/utils/session.server";
@@ -6,6 +6,13 @@ import stylesUrl from "../styles/login.css";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Remix Jokes | Login",
+    description: "Login to submit your own jokes to Remix Jokes!",
+  };
 };
 
 function validateUsername(username: unknown) {
@@ -63,7 +70,6 @@ export const action: ActionFunction = async ({ request }) => {
   switch (loginType) {
     case "login": {
       const user = await login({ username, password });
-      console.log({ user });
       if (!user) {
         return badRequest({
           fields,
@@ -86,7 +92,7 @@ export const action: ActionFunction = async ({ request }) => {
       if (!user) {
         return badRequest({
           fields,
-          formError: `Something went wrong trying to create new user.`,
+          formError: `Something went wrong trying to create a new user.`,
         });
       }
       return createUserSession(user.id, redirectTo);
