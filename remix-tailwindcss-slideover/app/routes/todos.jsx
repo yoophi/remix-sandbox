13ab914ba3bded05bef783d2708fb5927c3ca691
Pyunghyuk Yoo <yoophi@gmail.com>
2022-01-28 18:13:50 +0900

    ✨ UPDATE: close button is a link to /todos

--- remix-tailwindcss-slideover/app/routes/todos.jsx
+++ remix-tailwindcss-slideover/app/routes/todos.jsx
@@ -62,14 +62,15 @@ function TodoLayout() {
                             {todo?.title ? todo.title : "Panel title"}
                           </Dialog.Title>
                           <div className="ml-3 h-7 flex items-center">
-                            <button
-                              type="button"
-                              className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                              onClick={() => setOpen(false)}
-                            >
-                              <span className="sr-only">Close panel</span>
-                              <XIcon className="h-6 w-6" aria-hidden="true" />
-                            </button>
+                            <Link to="/todos">
+                              <button
+                                type="button"
+                                className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                              >
+                                <span className="sr-only">Close panel</span>
+                                <XIcon className="h-6 w-6" aria-hidden="true" />
+                              </button>
+                            </Link>
                           </div>
                         </div>
                       </div>
--- remix-tailwindcss-slideover/app/routes/todos/index.jsx
+++ remix-tailwindcss-slideover/app/routes/todos/index.jsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import { useTodoData } from "../../shared/context/todo";
+
 export default function TodoIndexRoute() {
+  const { setOpen } = useTodoData();
+  useEffect(() => {
+    setOpen(false);
+  }, []);
+
   return null;
 }
